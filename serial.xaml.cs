8d59f6dc1a41c4b0768705cb263eca72001de8ba
Joe Raven <joakim.ringstad.raavi@gmail.com>
2019-02-03 22:56:24 +0100

    Update serial.xaml.cs

--- serial.xaml.cs
+++ serial.xaml.cs
@@ -15,6 +15,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.Threading.Tasks;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,29 +29,70 @@ namespace serialbad
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int counter = 0;
         public MainPage()
         {
             this.InitializeComponent();
+            
+            
+        }
+        public async void initPorts1() {
+            
+        }
+
+        public async Task initPorts() {
+            
+            string selector = SerialDevice.GetDeviceSelector();
+           
+            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
+            DeviceInformation deviceInfo = devices[0];
+            Debug.WriteLine("Devices.Count: "+devices.Count);
+            counter = 0;
+            //serialDevice.Dispose();
+            foreach (var info in devices)
+            {
+                
+                SerialDevice serialDevice = await SerialDevice.FromIdAsync(info.Id);
+                
+                if (serialDevice != null)
+                {
+
+                    var port = serialDevice.PortName.ToString();
+                    comPortList.Items.Add(port.ToString());
+                    Debug.WriteLine(port.ToString());
+                    
+                }
+                counter++;
+                if (devices.Count == counter+1)
+                {                    
+                    Debug.WriteLine("Counter: "+counter);
+                    serialDevice.Dispose();
+                }
+
+                
+
+                
+
+
+            }
+
+           
+
+
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            comPortList.Items.Clear();
             initPorts();
         }
 
-        public async void initPorts() {
-            string selector = SerialDevice.GetDeviceSelector();
-            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
-            DeviceInformation deviceInfo = devices[0];
-            foreach (var info in devices)
-            {
-                SerialDevice serialDevice = await SerialDevice.FromIdAsync(info.Id);
-                if (serialDevice != null)
-                {
-                    var port = serialDevice.PortName.ToString();
-                    Debug.WriteLine(port);
-                    comPortList.Items.Add(port.ToString());
-                }
-            }
+        private void Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
-
-
     }
 
 
